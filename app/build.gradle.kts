5594af8c28646e2c5651dc4fa7ad7bb30ccc3f5c
five82 <five82@users.noreply.github.com>
2026-08-02 13:39:16 -0400

    Update Android build dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "xyz.five82.takeup"
         minSdk = 31
         targetSdk = 37
-        versionCode = 10
-        versionName = "0.6.0"
+        versionCode = 11
+        versionName = "0.6.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
